034cd277ba2e726fbf9e467730d6c87540f7777b
asifiqbalsagor <asifiqbalsagor98@gmail.com>
2022-11-11 15:07:26 +0600

    added header and footer

---
+++ Assignment 11/the-cooker-life/src/App.js
@@ -0,0 +1,34 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import Home from "./Home/Home";
+import Main from "./Layout/Main";
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children: [
+        {
+          path: "/",
+        element:<Home></Home>
+        },
+        {
+          path: "/",
+        element:<Home></Home>
+        },
+        {
+          path: "/",
+        element:<Home></Home>
+        }
+      ]
+    }
+  ])
+  return (
+    <div className="App">
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
+}
+
+export default App;
