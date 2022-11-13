62b07e12693e4567aa536228489417a892a36702
asifiqbalsagor <asifiqbalsagor98@gmail.com>
2022-11-13 16:30:06 +0600

    new change

--- Assignment 11/the-cooker-life/src/Home/Banner.js
+++ Assignment 11/the-cooker-life/src/Home/Banner.js
@@ -21,37 +21,11 @@ const Banner = () => {
         </div>
         <div className="relative flex flex-col items-start w-full max-w-xl px-4 mx-auto md:px-0 lg:px-8 lg:max-w-screen-xl">
           <div className="mb-16 lg:my-40 lg:max-w-lg lg:pr-5">
-            <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
-              Brand new
+            <h1 className="text-5xl">Welcome My The Cooker life Website.</h1>
+            <p className="pr-5 mb-5 text-base text-gray-700 md:text-lg my-6">
+            Here is my home made Best Weight Loss Eating Patterns for Men, Special Coffee, Chicken Fry,Vegetable Egg Ludus , Briyani etc.
+            I provide homemade food service.
             </p>
-            <h2 className="mb-5 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
-              Everything you
-              <br className="hidden md:block" />
-              can imagine{" "}
-              <span className="inline-block text-deep-purple-accent-400">
-                is real
-              </span>
-            </h2>
-            <p className="pr-5 mb-5 text-base text-gray-700 md:text-lg">
-              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-              quae. explicabo.
-            </p>
-            <div className="flex items-center">
-              <a
-                href="/"
-                className="inline-flex items-center justify-center h-12 px-6 mr-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
-              >
-                Get started
-              </a>
-              <a
-                href="/"
-                aria-label=""
-                className="inline-flex items-center font-semibold text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-700"
-              >
-                Learn more
-              </a>
-            </div>
           </div>
         </div>
       </div>
